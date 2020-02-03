1b047c5e97a992c19aad33c63f2305687f744316
bimonestle <bimonestle@yahoo.co.id>
2020-02-03 20:17:10 +0700

    add linkNodes() to get every single <a> node pfrom an HTML Document

--- link/parse.go
+++ link/parse.go
@@ -19,12 +19,43 @@ type Link struct {
 // Parse will take in an HTML document and will return
 // a slice of links parsed from it.
 func Parse(r io.Reader) ([]Link, error) {
+
+	// 1. Find <a> nodes in an HTML Document
+	// 2. foreach link node
+	// 	2a. Store value to a Link struct
+	// 3. return the Links
 	doc, err := html.Parse(r)
 	if err != nil {
 		return nil, err
 	}
-	dfs(doc, "")
+	nodes := linkNodes(doc)
+	for _, node := range nodes {
+		fmt.Println(node)
+	}
+	// This dfs() will return every single nodes of an
+	// HTML Document
+	// dfs(doc, "")
 	return nil, nil
+}
+
+// Get every single <a> node / element
+// from an HTML Document
+func linkNodes(n *html.Node) []*html.Node {
+
+	// This section checks:
+	// if the node is an <a> already, return it.
+	if n.Type == html.ElementNode && n.Data == "a" {
+		return []*html.Node{n}
+	}
+
+	// Otherwise it has to go through the
+	// whole document / every single node and check
+	// if it's a link
+	var ret []*html.Node
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ret = append(ret, linkNodes(c)...)
+	}
+	return ret
 }
 
 // Depth First Search algorithm.
