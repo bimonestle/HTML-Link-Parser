2bccb255b31bfcd2b1823e4e0e8efd65dd602d3d
bimonestle <bimonestle@yahoo.co.id>
2020-02-03 19:40:23 +0700

    Add dfs() to check every single nodes of an HTML Document

--- link/examples/ex1/main.go
+++ link/examples/ex1/main.go
@@ -12,6 +12,7 @@ var exampleHTML = `
 <body>
   <h1>Hello!</h1>
   <a href="/other-page">A link to another page</a>
+  <a href="/second-page">Link to second page</a>
 </body>
 </html>
 `
--- link/parse.go
+++ link/parse.go
@@ -1,6 +1,11 @@
 package link
 
-import "io"
+import (
+	"fmt"
+	"io"
+
+	"golang.org/x/net/html"
+)
 
 // Link represent a link (<a href="...") in an HTML document.
 type Link struct {
@@ -14,5 +19,26 @@ type Link struct {
 // Parse will take in an HTML document and will return
 // a slice of links parsed from it.
 func Parse(r io.Reader) ([]Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+	dfs(doc, "")
 	return nil, nil
+}
+
+// Depth First Search algorithm.
+// Will return every single nodes of HTML Document
+func dfs(n *html.Node, padding string) {
+	msg := n.Data
+	if n.Type == html.ElementNode {
+		msg = "<" + msg + ">"
+	}
+	fmt.Println(padding, msg)
+
+	// This loop will check every single nodes
+	// from root element until it hits the last element
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		dfs(c, padding+"  ")
+	}
 }
